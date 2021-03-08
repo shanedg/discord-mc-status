395114a6f3b69add923ef7c0185915ca9f8fb147
Shane Garrity <shanedgarrity@gmail.com>
2021-03-08 01:33:06 -0500

    progress: placeholder commands and comments

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/**
+ * A Discord app with a Bot user that can start and stop a Minecraft server running on a templated EC2 instance:
+ * https://discord.com/developers/applications/818225796924702740/bot
+ * Constructed by following this guide:
+ * https://discordjs.guide/
+ */
+
+const { exec } = require('child_process');
+
 const discord = require('discord.js');
 const dotenv = require('dotenv');
 
@@ -30,18 +39,50 @@ client.on('message', message => {
     case 'start':
       console.log('TODO: start server');
       message.channel.send('TODO: start server');
+      // TODO: check if server running
+      //  aws ec2 run-instances --launch-template LaunchTemplateId=lt-0b091f225fe894a12
+      exec('echo this would be the command', (error, stdout, stderr) => {
+        if (error) {
+          console.log('error', error.message);
+          return;
+        }
+        if (stderr) {
+          console.log('stderr', stderr);
+          return;
+        }
+        console.log('stdout', stdout);
+      });
+      // TODO: capture instance id
       break;
     case 'stop':
       console.log('TODO: stop server');
       message.channel.send('TODO: stop server');
+      // TODO: broadcast 30 second warning
+      // TODO: stop mc server
+      // TODO: backup
+      // terminate
+      // https://docs.aws.amazon.com/cli/latest/reference/ec2/terminate-instances.html
+      // aws ec2 terminate-instances --instance-ids i-0904ba119863694ff
       break;
     case 'status':
       console.log('TODO: get server status');
       message.channel.send('TODO: get server status');
+      // TODO: get instance status
+      // TODO: get server status
+      break;
+    case 'online':
+      console.log('TODO: whos online');
+      message.channel.send('TODO: whos online');
+      // TODO: get server players
       break;
     case 'backup':
       console.log('TODO: backup server');
       message.channel.send('TODO: backup server');
+      // TODO: broadcast 30 second warning
+      // TODO: turn off autosave
+      // TODO: backup
+      // TODO: turn on autosave
+      // TODO: broadcast backup complete
       break;
     default:
       console.log('wot?');
