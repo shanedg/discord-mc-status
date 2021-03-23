9645bfb398d11fc4077256b0c9a792568d074178
Shane Garrity <shanedgarrity@gmail.com>
2021-03-23 01:54:47 -0400

    test(rcon): add makeshift integration test

---
+++ libraries/minecraft-rcon/src/MinecraftRcon.test.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+/**
+ * System under test
+ */
+import MinecraftRcon from './MinecraftRcon.js';
+
+/**
+ * Configure test environment
+ */
+dotenv.config();
+const {
+  RCON_HOST: host,
+  RCON_PORT: port,
+  RCON_SECRET: password,
+} = process.env;
+
+const minecraft = new MinecraftRcon({ host, port, password });
+
+const exerciseRunAll = async () => {
+  const results = await minecraft.runAll([
+    'list',
+    'say hello everyone',
+    'tell @r hello to you in particular, random!',
+    'save-off',
+    'save-on',
+  ]);
+  console.log('runAll:', results);
+};
+
+const exercisePlayersOnline = async () => {
+  const playersOnline = await minecraft.getPlayersOnline();
+  console.log('online:', playersOnline);
+};
+
+await exerciseRunAll();
+await exercisePlayersOnline();
