ee5a7714634ede3b599160fa8148e73cb7cfd4d5
Shane Garrity <shanedgarrity@gmail.com>
2021-04-01 02:56:07 -0400

    style(bot): eslint --fix

--- apps/discord-bot/src/index.js
+++ apps/discord-bot/src/index.js
@@ -39,53 +39,53 @@ bot.on('message', message => {
   const command = content.replace(watchWordPattern, '');
 
   switch (command) {
-    case 'start':
-      message.channel.send('starting server...');
-      // TODO: check if server running
-      // TODO: capture instance id
-      // TODO: associate elastic ip with new instance
+  case 'start':
+    message.channel.send('starting server...');
+    // TODO: check if server running
+    // TODO: capture instance id
+    // TODO: associate elastic ip with new instance
 
-      // TODO: start?
-      //  aws ec2 run-instances --launch-template LaunchTemplateId=lt-0b091f225fe894a12
+    // TODO: start?
+    //  aws ec2 run-instances --launch-template LaunchTemplateId=lt-0b091f225fe894a12
 
-      break;
-    case 'stop':
-      message.channel.send('stopping server...');
-      axiosInstance.post('/stop')
-        .then(() => {
-          message.channel.send('server stopped.');
-        })
-        .catch(({ response }) => {
-          if (response?.status === 400) {
-            message.channel.send('connection refused, the server is (probably) already stopped.');
-          } else {
-            message.channel.send('there was an unexpected problem stopping the server!');
-          }
-        });
-      break;
-    case 'online':
-      axiosInstance.get('/online')
-        .then(({ data }) => {
-          message.channel.send(`online: ${data.length}/20\n${data.join(',')}`);
-        })
-        .catch(({ response }) => {
-          if (response?.status === 400) {
-            message.channel.send('connection refused, the server is probably stopped.');
-          } else {
-            message.channel.send('there was an unexpected problem checking who\'s online!');
-          }
-        });
-      break;
-    case 'backup':
-      message.channel.send('starting backup tasks...');
-      // TODO: broadcast 30 second warning
-      // TODO: turn off autosave
-      // TODO: backup
-      // TODO: turn on autosave
-      // TODO: broadcast backup complete
-      break;
-    default:
-      message.channel.send('sorry?');
+    break;
+  case 'stop':
+    message.channel.send('stopping server...');
+    axiosInstance.post('/stop')
+      .then(() => {
+        message.channel.send('server stopped.');
+      })
+      .catch(({ response }) => {
+        if (response?.status === 400) {
+          message.channel.send('connection refused, the server is (probably) already stopped.');
+        } else {
+          message.channel.send('there was an unexpected problem stopping the server!');
+        }
+      });
+    break;
+  case 'online':
+    axiosInstance.get('/online')
+      .then(({ data }) => {
+        message.channel.send(`online: ${data.length}/20\n${data.join(',')}`);
+      })
+      .catch(({ response }) => {
+        if (response?.status === 400) {
+          message.channel.send('connection refused, the server is probably stopped.');
+        } else {
+          message.channel.send('there was an unexpected problem checking who\'s online!');
+        }
+      });
+    break;
+  case 'backup':
+    message.channel.send('starting backup tasks...');
+    // TODO: broadcast 30 second warning
+    // TODO: turn off autosave
+    // TODO: backup
+    // TODO: turn on autosave
+    // TODO: broadcast backup complete
+    break;
+  default:
+    message.channel.send('sorry?');
   }
 });
 
