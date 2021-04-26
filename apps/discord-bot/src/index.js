216591c5f253547ab1a0afb72574823ccddccdc5
Shane Garrity <shanedgarrity@gmail.com>
2021-04-26 03:37:10 -0400

    fix(bot): handle systemd stop signal

--- apps/discord-bot/src/index.js
+++ apps/discord-bot/src/index.js
@@ -51,6 +51,7 @@ const saveOnExit = () => {
 
 process.on('SIGINT', saveOnExit); // Ctrl-c
 process.on('SIGUSR2', saveOnExit); // nodemon signal
+process.on('SIGTERM', saveOnExit); // systemd 'stop' signal
 process.on('exit', saveOnExit); // any exit?
 
 exec('cat .temp-last-instance-id', (error, stdout, stderr) => {
